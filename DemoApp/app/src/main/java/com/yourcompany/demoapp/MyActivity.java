b183b025ae4b125d962bdacdecba5aced061c36a
Tang Xiao Wei <xwtang@nltechdev.com>
2016-04-08 09:11:41 +0800

    Product Backlog Item 15777:Publish Android SDK to Github - step 1 for testing

--- DemoApp/app/src/main/java/com/yourcompany/demoapp/MyActivity.java
+++ DemoApp/app/src/main/java/com/yourcompany/demoapp/MyActivity.java
@@ -1,24 +1,30 @@
 package com.yourcompany.demoapp;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.View;
 
 import com.taylorcorp.lifepics.MainApplication;
-import com.taylorcorp.lifepics.listeners.OrderStatusListener;
-import com.taylorcorp.lifepics.lp.LifePicsResponse;
-import com.taylorcorp.lifepics.model.purchases.Cart;
-import com.taylorcorp.lifepics.products.ProductsActivity;
-import com.taylorcorp.lifepics.webservices.LifePicsWebServiceResponse;
-import com.taylorcorp.lifepics.webservices.data.AccountInfo;
+import com.taylorcorp.lifepics.activities.BaseLeftMenuActivity;
 
-public class MyActivity extends ActionBarActivity implements OrderStatusListener {
-    private String TAG = "MyActivity";
+public class MyActivity extends BaseLeftMenuActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mDrawer.setContentView(R.layout.lp_activity_base_single_fragment);
+
+        MainApplication.loadBannedStores(this);
+        configureView();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
 
     private void configureView() {
         FragmentManager fm = getSupportFragmentManager();
@@ -33,91 +39,32 @@ public class MyActivity extends ActionBarActivity implements OrderStatusListener
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-
-        // listen in
-        MainApplication.getShoppingCart().setOrderStatusListener(this);
-        String userID = MainApplication.getAppPreferences().getUserID();
-
-        if (userID == null || userID.isEmpty()) {
-            MainApplication.getLifePicsWebService().createTemporaryUser(
-                    MainApplication.getAppPreferences().getDeveloperKey(),
-                    MainApplication.getAppPreferences().getMerchantID(), new LifePicsWebServiceResponse() {
-                        @Override
-                        public void resultHandler(boolean isSuccess, Object response, com.taylorcorp.lifepics.webservices.entities.Error error, String message) {
-                            if (isSuccess) {
-                                AccountInfo info = (AccountInfo) response;
-                                MainApplication.setAccountInfo(info);
-                                MainApplication.getAppPreferences().setUserID(info.getUserId());
-                            } else {
-                                if (MainApplication.isDebug()) {
-                                    //Log.e("LP", error.mMessage);
-                                }
-                            }
-
-                            if (MainApplication.getAppPreferences().getMerchantID() != null &&
-                                    !MainApplication.getAppPreferences().getMerchantID().isEmpty())
-
-                                populateProducts();
-
-                            MainApplication.loadCart(MainApplication.getAppPreferences().getUserID(),
-                                    new LifePicsResponse() {
-                                        @Override
-                                        public void didComplete(Object response, Exception ex) {
-
-                                        }
-                                    });
-                        }
-
-                    });
-        } else {
-            if (MainApplication.getAppPreferences().getMerchantID() != null &&
-                    !MainApplication.getAppPreferences().getMerchantID().isEmpty()) {
-                populateProducts();
-
-                MainApplication.loadCart(MainApplication.getAppPreferences().getUserID(),
-                        new LifePicsResponse() {
-                            @Override
-                            public void didComplete(Object response, Exception ex) {
-
-                            }
-                        });
-            }
+    protected void homeClick(Activity currentActivity)
+    {
+        if((currentActivity instanceof MyActivity) && mDrawer.isMenuVisible())
+        {
+            mDrawer.closeMenu();
         }
-
-
-        if (MainApplication.isDebug())
-            Log.d("LP", "User id = " + MainApplication.getAppPreferences().getUserID());
-
-        configureView();
-    }
-
-    private void populateProducts() {
-        MainApplication.loadProducts(MainApplication.getMerchantID(), 1, new LifePicsResponse() {
-            @Override
-            public void didComplete(Object response, Exception ex) {
-
-            }
-        });
-    }
-
-    public void didClick(View v) {
-        MainApplication.getShoppingCart().clear();
-
-        Intent i = new Intent(this, ProductsActivity.class);
-        startActivity(i);
+        else
+        {
+            super.homeClick(currentActivity);
+        }
     }
 
     @Override
-    public void didSubmitOrder(Cart cart) {
-        Log.d(TAG, "Order was submitted with cart item count: " + cart.getCartItems().size());
+    protected void signOutClick(Activity currentActivity) {
+        if ((currentActivity instanceof MyActivity) && mDrawer.isMenuVisible()) {
+            mDrawer.closeMenu();
+        }
+        super.signOutClick(currentActivity);
     }
+
 
     @Override
-    public void didCancelOrder() {
-        Log.d(TAG, "Order was cancelled!");
+    protected void onResume()
+    {
+        super.onResume();
+        invalidateOptionsMenu();
     }
-
 }
+
--- DemoApp/app/src/main/java/com/yourcompany/demoapp/MyFragment.java
+++ DemoApp/app/src/main/java/com/yourcompany/demoapp/MyFragment.java
@@ -1,6 +1,8 @@
 package com.yourcompany.demoapp;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -9,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
@@ -21,9 +25,12 @@ import com.taylorcorp.lifepics.model.purchases.Cart;
 import com.taylorcorp.lifepics.model.purchases.Product;
 import com.taylorcorp.lifepics.model.sources.DeviceImageSource;
 import com.taylorcorp.lifepics.utils.AlertUtils;
+import com.taylorcorp.lifepics.webservices.LifePicsWebService;
 import com.taylorcorp.lifepics.webservices.LifePicsWebServiceResponse;
 import com.taylorcorp.lifepics.webservices.data.AccountInfo;
+import com.taylorcorp.lifepics.webservices.data.ProductInfo;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,8 +38,10 @@ import java.util.List;
  * Created by Andrew on 10/22/2014.
  */
 public class MyFragment extends Fragment implements OrderStatusListener {
+    private String TAG = "IntroFragment";
 
-    private String TAG = "MyFragment";
+    private Button mOrderButton;
+    private TextView mVersionTextView;
 
     private LifePicsPreferences mPreferences;
 
@@ -42,51 +51,75 @@ public class MyFragment extends Fragment implements OrderStatusListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.my_fragment, null);
+        View v = inflater.inflate(R.layout.lp_fragment_intro, null);
 
+        mVersionTextView = (TextView)v.findViewById(R.id.lp_txt_version);
+        mOrderButton = (Button)v.findViewById(R.id.lp_btn_order);
 
         mPreferences = new LifePicsPreferences(getActivity());
 
         configureView();
+        //checkIfWeNeedSomeLocalImages();
+        connectToLifePics();
 
         return v;
     }
 
+    private String loadProductsFromAssets() {
+        String json = null;
+        try {
+            InputStream is = getActivity().getAssets().open("products.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (Exception e) {
+            if (MainApplication.isDebug())
+                Log.e("LP", "Load product", e);
+        }
+        return json;
+    }
+
+    private void populateProducts() {
+        try {
+            String json = loadProductsFromAssets();
+
+            List<ProductInfo> plist = ProductInfo.parseList(json);
+
+            List<Product> products = new ArrayList<>();
+            for (ProductInfo p : plist) {
+                Product prod = new Product(p);
+                products.add(prod);
+            }
+
+            MainApplication.setProducts(products);
+        } catch (Exception e) {
+            if (MainApplication.isDebug())
+                Log.e("LP", "Load List", e);
+        }
+    }
+
     private void configureView() {
+        try {
+            String buildVersion = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
+            int buildVersionCode = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionCode;
+            String buildTarget = getString(R.string.lp_build_target);
+            String version = getString(R.string.lp_intro_version, buildVersion, buildVersionCode, buildTarget);
+
+            mVersionTextView.setText(version);
+
+            populateProducts();
+        } catch (PackageManager.NameNotFoundException ex) {
+            mVersionTextView.setText(getString(R.string.lp_intro_version_unknown));
+        }
+
         // listen in
         MainApplication.getShoppingCart().setOrderStatusListener(this);
-        MainApplication.getShoppingCart().setOrderStatusListener(this);
-
-        checkIfWeNeedSomeLocalImages();
-
         String userID = MainApplication.getAppPreferences().getUserID();
 
         if (userID == null || userID.isEmpty()) {
-            MainApplication.getLifePicsWebService().createTemporaryUser(
-                    MainApplication.getAppPreferences().getDeveloperKey(),
-                    MainApplication.getAppPreferences().getMerchantID(), new LifePicsWebServiceResponse() {
-                        @Override
-                        public void resultHandler(boolean isSuccess, Object response, com.taylorcorp.lifepics.webservices.entities.Error error, String message) {
-                            if (isSuccess) {
-                                AccountInfo info = (AccountInfo) response;
-                                MainApplication.setAccountInfo(info);
-                                MainApplication.getAppPreferences().setUserID(info.getUserId());
-                            } else {
-                                if (MainApplication.isDebug()) {
-                                    //Log.e("LP", error.mMessage);
-                                }
-                            }
 
-                            MainApplication.loadCart(MainApplication.getAppPreferences().getUserID(),
-                                    new LifePicsResponse() {
-                                        @Override
-                                        public void didComplete(Object response, Exception ex) {
-
-                                        }
-                                    });
-                        }
-
-                    });
         } else {
             if (MainApplication.getAppPreferences().getMerchantID() != null &&
                     !MainApplication.getAppPreferences().getMerchantID().isEmpty()) {
@@ -101,90 +134,42 @@ public class MyFragment extends Fragment implements OrderStatusListener {
             }
         }
 
-        if (MainApplication.getAppPreferences().getMerchantID() != null && !MainApplication.getAppPreferences().getMerchantID().isEmpty()) {
-            MainApplication.loadProducts(MainApplication.getAppPreferences().getMerchantID(), 1, new LifePicsResponse() {
 
-                @Override
-                public void didComplete(Object response, Exception ex) {
-                    if (MainApplication.isDebug()) {
-                        Log.d("LP", "Products Loaded");
-                    }
-                }
-            });
-        } else {
-            List<Product> products = new ArrayList<Product>();
-
-            products.add(Product.getGenericProduct(4.0, 6.0));
-            products.add(Product.getGenericProduct(4.0, 8.0));
-            products.add(Product.getGenericProduct(5.0, 7.0));
-            products.add(Product.getGenericProduct(8.0, 10.0));
-            products.add(Product.getGenericProduct(8.0, 12.0));
-            products.add(Product.getGenericProduct(11.0, 14.0));
-            products.add(Product.getGenericProduct(20.0, 16.0));
-            products.add(Product.getGenericProduct(30.0, 20.0));
-            products.add(Product.getGenericProduct(36.0, 24.0));
-            products.add(Product.getGenericProduct(4.0, 4.0));
-            products.add(Product.getGenericProduct(5.0, 5.0));
-            products.add(Product.getGenericProduct(8.0, 8.0));
-            products.add(Product.getGenericProduct(10.0, 10.0));
-
-
-            MainApplication.setProducts(products);
-
-        }
 
         if (MainApplication.isDebug())
             Log.d("LP", "User id = " + MainApplication.getAppPreferences().getUserID());
-    }
 
-    private void checkIfWeNeedSomeLocalImages() {
-        boolean isAnyDevicePhotos = DeviceImageSource.isAnyDevicePhotos(getActivity());
-        boolean hasAskedForLoremPixelPreviously = mPreferences.getAskForLoremPixelOnStartup();
-
-        if (isAnyDevicePhotos || !hasAskedForLoremPixelPreviously) {
-            return;
-        }
-
-        mPreferences.setAskForLoremPixelOnStartup(false);
-
-        AlertUtils.showOKCancelAlert(getActivity(), R.string.lp_intro_add_images_title, R.string.lp_intro_add_images_message, new DialogInterface.OnClickListener() {
+        mOrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                String[] resolutions = new String[]{
-                        "2448/3264", "3264/2448", "640/640", "1280/960", "960/1280", "300/300",
-                        "1280/720", "720/1280", "960/960", "640/960", "960/640", "100/100",
-                        "640/480", "480/640", "480/480", "1280/720", "720/1280", "640/640",
-                        "640/960", "960/640", "640/640", "640/960", "960/640", "640/640",
-                        "640/960", "960/640", "640/640", "640/960", "960/640", "640/640"
-                };
-
-                for (String iResolution : resolutions) {
-                    String url = String.format("http://lorempixel.com/%s", iResolution);
-
-                    ImageLoader.getInstance().loadImage(url, new ImageLoadingListener() {
-                        @Override
-                        public void onLoadingStarted(String imageUri, View view) {
-                        }
-
-                        @Override
-                        public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-                        }
-
-                        @Override
-                        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-                            try {
-                                MediaStore.Images.Media.insertImage(getActivity().getContentResolver(), loadedImage, "Lorem Pixel", "Added by Lorem Pixel");
-                            } catch (Exception ex) {
-                            }
-                        }
-
-                        @Override
-                        public void onLoadingCancelled(String imageUri, View view) {
-                        }
-                    });
-                }
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), com.taylorcorp.lifepics.products.ProductsActivity.class);
+                startActivity(i);
             }
         });
+
+        mOrderButton.setEnabled(false);
+    }
+
+    private void loadCart() {
+        MainApplication.loadCart(MainApplication.getAppPreferences().getUserID(), new LifePicsResponse() {
+            @Override
+            public void didComplete(Object response, Exception ex) {
+
+            }
+        });
+    }
+
+    private void connectToLifePics() {
+        // initiate connection to services to get a session
+        final LifePicsWebService service = new LifePicsWebService(getActivity());
+
+        mOrderButton.setEnabled(false);
+
+        String developerKey = getString(R.string.lp_developer_key);
+        if (developerKey != null) {
+            mOrderButton.setEnabled(true);
+            MainApplication.getAppPreferences().setDeveloperKey(developerKey);
+        }
     }
 
     @Override
@@ -197,3 +182,4 @@ public class MyFragment extends Fragment implements OrderStatusListener {
         Log.d(TAG, "Order was cancelled!");
     }
 }
+
