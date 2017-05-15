0e641ae6b7c59983ddc874cb85b991832db04352
lsw <lsw_wsl@outlook.com>
2017-05-15 08:53:53 +0800

    PBI 33450 - Sample codes for new LifePicsSDK.

--- DemoApp/app/src/main/java/com/yourcompany/demoapp/MyFragment.java
+++ DemoApp/app/src/main/java/com/yourcompany/demoapp/MyFragment.java
@@ -12,7 +12,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
@@ -25,13 +27,16 @@ import com.taylorcorp.lifepics.model.purchases.Cart;
 import com.taylorcorp.lifepics.model.purchases.Product;
 import com.taylorcorp.lifepics.model.sources.DeviceImageSource;
 import com.taylorcorp.lifepics.utils.AlertUtils;
+import com.taylorcorp.lifepics.utils.ResourceUtils;
 import com.taylorcorp.lifepics.webservices.LifePicsWebService;
 import com.taylorcorp.lifepics.webservices.LifePicsWebServiceResponse;
 import com.taylorcorp.lifepics.webservices.data.AccountInfo;
+import com.taylorcorp.lifepics.webservices.data.ProductCategoryInfo;
 import com.taylorcorp.lifepics.webservices.data.ProductInfo;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -40,7 +45,7 @@ import java.util.List;
 public class MyFragment extends Fragment implements OrderStatusListener {
     private String TAG = "IntroFragment";
 
-    private Button mOrderButton;
+    private Button btnTraditionalPrints, btnSquarePrints;
     private TextView mVersionTextView;
 
     private LifePicsPreferences mPreferences;
@@ -53,14 +58,13 @@ public class MyFragment extends Fragment implements OrderStatusListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.lp_fragment_intro, null);
 
-        mVersionTextView = (TextView)v.findViewById(R.id.lp_txt_version);
-        mOrderButton = (Button)v.findViewById(R.id.lp_btn_order);
+        mVersionTextView = (TextView) v.findViewById(R.id.lp_txt_version);
+        btnTraditionalPrints = (Button) v.findViewById(R.id.lp_btn_Traditional_Prints);
+        btnSquarePrints = (Button) v.findViewById(R.id.lp_btn_Square_Prints);
 
         mPreferences = new LifePicsPreferences(getActivity());
 
         configureView();
-        //checkIfWeNeedSomeLocalImages();
-        connectToLifePics();
 
         return v;
     }
@@ -134,42 +138,86 @@ public class MyFragment extends Fragment implements OrderStatusListener {
             }
         }
 
-
-
-        if (MainApplication.isDebug())
-            Log.d("LP", "User id = " + MainApplication.getAppPreferences().getUserID());
-
-        mOrderButton.setOnClickListener(new View.OnClickListener() {
+        btnTraditionalPrints.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String categoryName = "Traditional Prints";
                 Intent i = new Intent(getActivity(), com.taylorcorp.lifepics.products.ProductsActivity.class);
-                startActivity(i);
+                loadProducts(categoryName,i);
             }
         });
 
-        mOrderButton.setEnabled(false);
+        btnSquarePrints.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String categoryName = "Square Prints";
+                Intent i = new Intent(getActivity(), com.taylorcorp.lifepics.products.ProductsActivity.class);
+                loadProducts(categoryName,i);
+            }
+        });
     }
 
-    private void loadCart() {
-        MainApplication.loadCart(MainApplication.getAppPreferences().getUserID(), new LifePicsResponse() {
+    HashMap<String, ProductCategoryInfo> categoryNames;
+
+    private void loadProducts(final String category, final Intent intent) {
+
+        MainApplication.setMerchantID(getString(ResourceUtils.getString(getActivity(), "lp_merchant_id")));
+
+        final List<Product> finalList = new ArrayList<>();
+        MainApplication.loadProducts(MainApplication.getAppPreferences().getMerchantID(), 1, new LifePicsResponse() {
             @Override
             public void didComplete(Object response, Exception ex) {
 
+                try {
+                    final List<ProductInfo> fromWeb = (List<ProductInfo>) response;
+
+                    if (fromWeb.size() == 0) {
+                        Toast.makeText(getActivity(), "Product load failed, please try again later.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    String json;
+                    json = loadProductsFromAssets();
+
+                    try {
+
+                        categoryNames = ProductInfo.parseCategoryInfo(json);
+
+                        List<ProductInfo> plist = ProductInfo.parseList(json);
+                        for (ProductInfo p : plist) {
+                            if (p.getCategoryName().equals(category)) {
+                                for (ProductInfo fromWebProduct : fromWeb) {
+                                    if (fromWebProduct.getProductID() == p.getProductID()) {
+                                        p.setLengthResolution(fromWebProduct.getLengthResolution());
+                                        p.setWidthResolution(fromWebProduct.getWidthResolution());
+                                        p.setProductID(fromWebProduct.getProductID());
+
+                                        finalList.add(new Product(p));
+                                    }
+                                }
+
+                            }
+                        }
+
+                        MainApplication.setProductCategory(category);
+                        MainApplication.setProductCategoryInfo(categoryNames.get(category));
+                        MainApplication.setProducts(finalList);
+                        startActivity(intent);
+
+                    } catch (Exception e) {
+                        if (MainApplication.isDebug()) {
+                            Log.e("LP", "Exception", e);
+                        }
+                    }
+                } catch (Exception exx) {
+                    if (MainApplication.isDebug()) {
+                        Log.e("LP", "Exception", exx);
+                    }
+
+                    Toast.makeText(getActivity(), "Product load failed, please try again later.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-    }
-
-    private void connectToLifePics() {
-        // initiate connection to services to get a session
-        final LifePicsWebService service = new LifePicsWebService(getActivity());
-
-        mOrderButton.setEnabled(false);
-
-        String developerKey = getString(R.string.lp_developer_key);
-        if (developerKey != null) {
-            mOrderButton.setEnabled(true);
-            MainApplication.getAppPreferences().setDeveloperKey(developerKey);
-        }
     }
 
     @Override
